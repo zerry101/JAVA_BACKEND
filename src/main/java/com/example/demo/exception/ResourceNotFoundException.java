c935550b5f6a9e446bf7cba24fb7193659a56832
zerry101 <macone394@gmail.com>
2023-03-05 17:32:43 +0530

    m

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author macon
  */
 
-@ResponseStatus(value = HttpStatus.BAD_GATEWAY)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
     private static  final long serialVersionUID=1L;
     public ResourceNotFoundException(String messageString){
