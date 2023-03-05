e052eab03b6eb63e62f07747def1e521296ce74b
zerry101 <macone394@gmail.com>
2023-03-05 18:39:51 +0530

    added post method

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -14,6 +14,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,31 +23,28 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author macon
  */
-@CrossOrigin(origins = "http://localhost:4200/") 
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
-        
+
 //    System.OutOfMemoryError.println("")
-    
     @Autowired
     private EmployeeRepository employeeRepository;
-    
-//    get all Employees
 
+//    get all Employees
     /**
      *
      * @return
      */
     @GetMapping("/employees")
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
-        
     }
     
-    
-    
-//    @Autowired
-//    private EmployeeRepository employeeRepository;
-    
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+
 }
